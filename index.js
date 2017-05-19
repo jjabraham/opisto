202df8ad3d4002f30cc3ee2f6d546ad0943cfc1a
John J Abraham <john.abraham@yahoo.com>
2017-05-19 16:01:00 +1000

    cleanup index.js

--- index.js
+++ index.js
@@ -5,25 +5,17 @@ const mongoose = require('mongoose')
 const helmet = require('helmet')
 const debug = require('debug')('app-server')
 const compression = require('compression')
+const router = require('./router/router')
+const secureRouter = require('./router/secure-router')
 require('dotenv').config()
 
 const app = express()
 const port = process.env.APP_PORT
 const server = process.env.APP_SERVER
-// const auth = require('./lib/authentication')
-const router = require('./router/router')
-const secureRouter = require('./router/secure-router')
 
 mongoose.connect(`${process.env.DB_URI}${process.env.DB_NAME}`)
 
 app.use(helmet())
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials')
-//   res.header('Access-Control-Allow-Credentials', 'true')
-//   next()
-// })
 app.use(compression())
 app.use(bodyParser.json())
 
